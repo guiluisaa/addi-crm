329cf9f21f3f3e930d26c7455dfb732c1e3395ca
Guilherme Araujo <guiluis.aa@gmail.com>
2020-09-22 20:15:44 -0300

    chore: add redux-persist lib

--- src/io/redux/store.ts
+++ src/io/redux/store.ts
@@ -1,20 +1,33 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 
 import rootReducer from './root.reducer';
 
+const initialState = {};
 const middleware = [thunk];
 
 const composeOptions = [applyMiddleware(...middleware)];
 const reduxDevTools =
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
   (window as any).__REDUX_DEVTOOLS_EXTENSION__();
+
 if (
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
   process.env.ENVIRONMENT !== 'production'
-)
+) {
   composeOptions.push(reduxDevTools);
+}
 
-const store = createStore(rootReducer, compose(...composeOptions));
+const persistedReducer = persistReducer({ key: 'root', storage }, rootReducer);
 
-export default store;
+const store = createStore(
+  persistedReducer,
+  initialState,
+  compose(...composeOptions)
+);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
