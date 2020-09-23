cccb2254b89a45e2f8605b2032d1165cf74b4da5
Guilherme Araujo <guiluis.aa@gmail.com>
2020-09-23 19:33:47 -0300

    feat(api): registries mock api

---
+++ src/mock/registries.mock.ts
@@ -0,0 +1,11 @@
+import { PersonDto } from '@/dto/Person.dto';
+
+export const registries: PersonDto[] = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    idNumber: '1234',
+    birthdate: '1990-08-29',
+    email: 'johndoe@gmail.com',
+  },
+];
