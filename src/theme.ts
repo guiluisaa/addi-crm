f96bd24b2ede9ec15b2afb22c9608287f07b8c6e
Guilherme Araujo <guiluis.aa@gmail.com>
2020-09-23 22:06:16 -0300

    chore: font, colors and theme

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,39 @@ const theme: DefaultTheme = {
   color: {
     white: '#FFF',
     black: '#424242',
+
+    primary: '#6272a4',
+    secondary: '#44475a',
+
+    success: '#2AD178',
+    warning: '#FABD59',
+    danger: '#FF5B68',
+  },
+
+  font: {
+    family: {
+      primary: "'Open Sans', sans-serif",
+    },
+
+    weight: {
+      regular: 400,
+      semiBold: 600,
+    },
+
+    color: {
+      primary: '#424242',
+      secondary: '#969696',
+    },
+  },
+
+  borderRadius: {
+    default: '3px',
+  },
+
+  breakpoint: {
+    sm: 576,
+    md: 768,
+    lg: 992,
   },
 };
 
