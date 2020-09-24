b55b2822fd832011c3a927a5a73c8b9745665a49
Guilherme Araujo <guiluis.aa@gmail.com>
2020-09-23 22:06:16 -0300

    chore: font, colors and theme

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -5,6 +5,39 @@ declare module 'styled-components' {
     color: {
       white: string;
       black: string;
+
+      primary: string;
+      secondary: string;
+
+      success: string;
+      warning: string;
+      danger: string;
+    };
+
+    font: {
+      family: {
+        primary: string;
+      };
+
+      weight: {
+        regular: number;
+        semiBold: number;
+      };
+
+      color: {
+        primary: string;
+        secondary: string;
+      };
+    };
+
+    borderRadius: {
+      default: string;
+    };
+
+    breakpoint: {
+      sm: number;
+      md: number;
+      lg: number;
     };
   }
 }
