acbf811cf8fd56b8a1ea0f70efaff0df7086fadc
Guilherme Araujo <guiluis.aa@gmail.com>
2020-09-28 18:12:05 -0300

    impr: feedback text

--- src/mock/data/leads.mock.ts
+++ src/mock/data/leads.mock.ts
@@ -1,14 +1,30 @@
 import { LeadDto } from '@/models/Lead.model';
 import { persons } from './persons.mock';
 
-const validLeads: LeadDto[] = persons.map(person => ({
-  ...person,
+const validLeads: LeadDto[] = persons.map(person => {
+  const leadparams = {
+    existsInRegisty: null,
+    matchWithRegisty: null,
+    hasJudicialRecord: null,
+    score: 0,
+  };
 
-  existsInRegisty: null,
-  matchWithRegisty: null,
-  hasJudicialRecord: null,
-  score: 0,
-}));
+  if (person.firstName === 'Daniel')
+    return {
+      firstName: 'Daniela',
+      lastName: 'Smith',
+      nationalIdNumber: '19399063003',
+      birthdate: '1972-01-15',
+      email: 'danielsmith@gmail.com',
+
+      ...leadparams,
+    };
+
+  return {
+    ...person,
+    ...leadparams,
+  };
+});
 
 const invalidLeads: LeadDto[] = [
   {
