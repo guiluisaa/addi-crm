d7ca3cae8fe8b99c990bffc11a6a136e6c61ecbb
Guilherme Araujo <guiluis.aa@gmail.com>
2020-09-28 00:04:07 -0300

    feat: prospectors table list

--- src/views/index/Index.view.tsx
+++ src/views/index/Index.view.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react';
 
 import LeadsSection from '@/components/lead/LeadsSection.component';
+import ProspectorsSection from '@/components/prospector/ProspectorsSection.component';
 
 const IndexView: FC = () => (
   <>
     <LeadsSection />
+
+    <ProspectorsSection />
   </>
 );
 
